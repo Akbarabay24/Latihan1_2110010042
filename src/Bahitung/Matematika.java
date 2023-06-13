4126fc5670dc3a8d6a770502a771628010b18b91
unknown <akbarfurqoni24@gmail.com>
2023-06-13 08:24:42 -0700

    first commit

---
+++ src/Bahitung/Matematika.java
@@ -0,0 +1,24 @@
+package Bahitung;
+public class Matematika {
+    private double bil1, bil2;
+    
+    public Matematika(double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    public double setPenjumlahan(){
+        return bil1 + bil2;
+    }
+    
+    public double setPengurangan(){
+        return bil1 - bil2;
+    }
+    
+    public double setPerkalian(){
+        return bil1 * bil2;
+    }
+    
+    public double setPembagian(){
+        return bil1 / bil2;
+    }
+}
